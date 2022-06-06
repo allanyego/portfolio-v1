953fbd44891b67872cb00a8f08b4754360f2071e
Yego Allan <allanyego05@gmail.com>
2022-06-06 08:54:54 +0000

    Setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { Button } from "semantic-ui-react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Button>Try Me!</Button>
+    </div>
+  );
+}
+
+export default App;
